af74690052190bcc5d5910e553de638c3e5cd715
AryBotz <101872515+arybotzganteng@users.noreply.github.com>
2022-04-15 00:04:21 +0800

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ let chalk = require('chalk')
 
 global.linkGC = ['https://chat.whatsapp.com/EVCGfzxLWfp81n0WhmVklH', 'https://chat.whatsapp.com/FnNAbem8o6r4pgLhSdO8Q9', 'https://chat.whatsapp.com/HBhy7rVae3o0PkIfkhvs2N'] // ganti jadi group lu
 global.owner = ['79519874382'] // Masukan nomot kalian
-global.mods = ['6289509772360'] // Moderator
+global.mods = ['79519874382'] // Moderator
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
   // name: 'https://website'
