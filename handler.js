e600c1d6139c1b86b1a8bdee3ef46d3e5b8764e7
AryBotz <101872515+arybotzganteng@users.noreply.github.com>
2022-04-15 12:20:33 +0800

    Update handler.js

--- handler.js
+++ handler.js
@@ -571,7 +571,7 @@ module.exports = {
                 .setBackground("https://telegra.ph/file/89a6260f0a6720240e698.jpg")
                 .toAttachment()
 
-              this.sendButtonImg(jid, action === 'add' ? wel.toBuffer() : lea.toBuffer(), text, action === 'add' ? 'Welcome Message' : 'Leave Message', action === 'add' ? 'Welcome beban🚬' : 'Alhamdullilah Beban Out🗿',action === 'add' ? 'Welcome Beban🚬' : 'Alhamdullilah Beban Out🗿', {
+              this.sendButtonImg(jid, action === 'add' ? wel.toBuffer() : lea.toBuffer(), text, action === 'add' ? 'Welcome Message' : 'Leave Message', action === 'add' ? 'Welcome Pacar/Teman ku 🥀' : 'Akhirnya Beban Out🗿',action === 'add' ? 'Welcome Pacar/Teman ku 🥀' : 'Akhirnya Beban Out🗿', {
 key: {
 fromMe: false,
 participant: '0@s.whatsapp.net',
